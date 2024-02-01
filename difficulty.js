a0fd2f3fd39b98ab12231da5e43f4a3676ba48f6
ShivaGav <152566381+ShivaGav@users.noreply.github.com>
2024-02-01 18:28:43 +0800

    Add files via upload

---
+++ difficulty.js
@@ -0,0 +1,17 @@
+document.getElementById('easy').addEventListener('click', function() {
+    setTimeout(function() {
+        window.location.href = 'ques.html';
+    }, 100);
+});
+
+document.getElementById('mode').addEventListener('click', function() {
+    setTimeout(function() {
+        window.location.href = 'mode.html';
+    }, 100);
+});
+
+document.getElementById('diff').addEventListener('click', function() {
+    setTimeout(function() {
+        window.location.href = 'diff.html';
+    }, 100);
+});
---
+++ menu.js
@@ -0,0 +1,11 @@
+document.getElementById('startGame').addEventListener('click', function() {
+    setTimeout(function() {
+        window.location.href = 'difficulty.html';
+    }, 100);
+});
+
+document.getElementById('easy').addEventListener('click', function() {
+    setTimeout(function() {
+        window.location.href = 'ques.html';
+    }, 100);
+});
